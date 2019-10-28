6894127641e159b6d924c0ba7816ccd8fab11ca0
celloguy <will_wright24@yahoo.com>
2019-10-27 22:53:15 -0500

    Initial Setup

---
+++ Assets/IsometricFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricFollow : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + Vector3.back * 10 + Vector3.up*5;
+    }
+}
